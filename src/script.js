a5526d88e9569dfbe7afbfe4f98f668eafd72aa6
justNuka <75932540+justNuka@users.noreply.github.com>
2023-04-11 10:47:18 +0200

    1er

--- src/script.js
+++ src/script.js
@@ -1,89 +1,109 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-var birdX = canvas.width / 4;
-var birdY = canvas.height / 2;
-var birdDY = 0;
+const canvasWidth = 400;
+const canvasHeight = 500;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+let birdX = canvasWidth / 4;
+let birdY = canvasHeight / 2;
+let birdDY = 0;
 const birdSize = 20;
 
-var pipeX = canvas.width;
-var pipeGap = 100;
-var pipeDY = -1.5;
-const pipeWidth = 30;
-var pipeHeight = canvas.height - pipeGap - 50;
-var pipePassed = false;
+let pipeX = canvasWidth;
+let pipeGap = 100;
+let pipeDY = -1.5;
+const pipeWidth = 50;
+let pipeHeight = canvasHeight - pipeGap - 50;
+let pipePassed = false;
 
-var score = 0;
+let score = 0;
 
-var intervalID;
+let intervalID;
+
+let isGameOver = false;
+
+const gravity = 0.1;
 
 function drawBird() {
   ctx.beginPath();
   ctx.fillStyle = "yellow";
-  ctx.arc(birdX, birdY, birdSize, 0, Math.PI * 2);
+  ctx.arc(birdX, birdY, birdSize, 0, 2 * Math.PI);
   ctx.fill();
 }
 
 function drawPipe() {
   ctx.fillStyle = "green";
   ctx.fillRect(pipeX, 0, pipeWidth, pipeHeight);
-  ctx.fillRect(pipeX, pipeHeight + pipeGap, pipeWidth, canvas.height - pipeGap - pipeHeight);
+  ctx.fillRect(pipeX, pipeHeight + pipeGap, pipeWidth, canvasHeight - pipeHeight - pipeGap);
 }
 
 function drawScore() {
-  ctx.font = "20px Arial";
   ctx.fillStyle = "black";
+  ctx.font = "24px Arial";
   ctx.fillText("Score: " + score, 10, 30);
 }
 
 function updateBird() {
-  birdDY += 0.2;
+  birdDY += gravity;
   birdY += birdDY;
-  if (birdY < birdSize || birdY > canvas.height - birdSize) {
-    gameOver();
-  }
 }
 
 function updatePipe() {
-  pipeX -= 2;
-  if (pipeX < -pipeWidth) {
-    pipeX = canvas.width;
-    pipeHeight = Math.floor(Math.random() * (canvas.height - pipeGap - 50)) + 50;
+  pipeX -= 1;
+
+  if (pipeX <= -pipeWidth) {
+    pipeX = canvasWidth;
+    pipeHeight = Math.floor(Math.random() * (canvasHeight - pipeGap - 100)) + 50;
     pipePassed = false;
   }
-  if (pipeX < birdX && !pipePassed) {
+
+  if (pipeX === birdX - pipeWidth / 2) {
     score++;
     pipePassed = true;
   }
-  if (birdX + birdSize > pipeX && birdX - birdSize < pipeX + pipeWidth &&
-      (birdY - birdSize < pipeHeight || birdY + birdSize > pipeHeight + pipeGap)) {
+
+  // Check collision
+  if (
+    birdX + birdSize > pipeX &&
+    birdX - birdSize < pipeX + pipeWidth &&
+    (birdY - birdSize < pipeHeight || birdY + birdSize > pipeHeight + pipeGap)
+  ) {
     gameOver();
   }
 }
 
 function gameOver() {
-  document.getElementById("game-over").innerHTML = "Game Over! Your score is " + score;
+  isGameOver = true;
   clearInterval(intervalID);
 }
 
-
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBird();
-  drawPipe();
-  drawScore();
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
   updateBird();
   updatePipe();
+
+  drawPipe();
+  drawBird();
+  drawScore();
+
+  if (birdY + birdSize > canvasHeight || birdY - birdSize < 0) {
+    gameOver();
+  }
+
+  if (isGameOver) {
+    ctx.fillStyle = "red";
+    ctx.font = "48px Arial";
+    ctx.fillText("Game Over", canvasWidth / 2 - 120, canvasHeight / 2);
+  }
 }
 
-document.addEventListener("keydown", function(event) {
+intervalID = setInterval(draw, 10);
+
+document.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
-    birdDY = -5;
+    birdDY = -3;
   }
 });
-
-canvas.addEventListener("click", function() {
-  birdDY = -5;
-});
-
-intervalID = setInterval(draw, 10);
